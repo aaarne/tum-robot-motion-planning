c0ae004d6d67da9f92766c3b2b9981534cfa8175
Arne Sachtler <arne.sachtler@gmail.com>
2019-05-16 00:35:34 +0200

    Add breeze to project

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,18 @@
+import breeze.linalg._
+import breeze.plot._
+
 object Main extends App {
-  println("Hello, World!")
+
+  val f = Figure("Test")
+  val x = linspace(0.0, 1.0)
+
+  f.subplot(1, 2, 0).title = "Some Lines"
+  f.subplot(1, 2, 1).title = "Fancy image"
+
+  f subplot 0 += plot(x, x ^:^ 2.0)
+  f subplot 0 += plot(x, x ^:^ 3.0)
+
+  f subplot 1 += image(DenseMatrix.rand(200, 200))
+
+  f.refresh()
 }
