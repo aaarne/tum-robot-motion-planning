d34473556ea0af147ad8e66b66e77eeb9c890cd2
Arne Sachtler <arne.sachtler@gmail.com>
2019-05-18 16:39:37 +0200

    Remove redundant case class

--- src/main/scala/ex1/configuration_space/ConfigurationSpace.scala
+++ src/main/scala/ex1/configuration_space/ConfigurationSpace.scala
@@ -6,31 +6,33 @@ import breeze.plot._
 
 class ConfigurationSpace extends CollisionChecker with RandomRects {
 
-  val robotSpec = new Robot(List(1, 1))
-  val rects = createRandomRects(2).toList
-
   lazy val confspace: DenseMatrix[Double] = {
     val q1 = toRadians(linspace(0, 180, 181))
     val q2 = toRadians(linspace(0, 359, 360))
 
     DenseMatrix.tabulate(q1.size, q2.size){
-      case (i, j) => if (checkCollision(robotSpec moveTo List(q1(i), q2(j)), rects)) 0.0 else 1.0
+      case (i, j) => if (checkCollision(robot moveTo List(q1(i), q2(j)), rects)) 0.0 else 1.0
     }
   }
+  val rects: List[Rectangle] = createRandomRects(2).toList
+  val robot = new Robot(List(1, 1))
 }
 
 class ShowConfigurationSpace extends ConfigurationSpace with Plotter {
 
   val f = Figure("Robot Visualizer")
 
-  implicit val p = f subplot 0
-  plotRobot(robotSpec moveTo List(.25*math.Pi, .25*math.Pi))
+  implicit val p: Plot = f subplot 0
+  plotRobot(robot moveTo List(.25 * math.Pi,.25 * math.Pi))
   rects foreach plotRectangle
 
   println(s"Percentage of confspace collision-free: ${100*sum(confspace)/confspace.size}%")
 
   val f2 = Figure("Configuration Space")
-  f2 subplot 0 += image(-1.0 * confspace.t)
+  val p2: Plot = f2 subplot 0
+  p2 += image(-1.0 * confspace.t)
+  p2.xlabel = "q1"
+  p2.ylabel = "q2"
 
   f.refresh()
   f2.refresh()
--- src/main/scala/ex1/configuration_space/Robot.scala
+++ src/main/scala/ex1/configuration_space/Robot.scala
@@ -8,34 +8,30 @@ class Robot(linkLengths: List[Double]) {
 
   type Frame = Matrix[Double]
 
+  def points(jointAngles: List[Double]): List[Vector[Double]] = extractPoints(frames(jointAngles))
+
   def frames(jointAngles: List[Double]): List[Frame] = {
 
-    case class Link(jointAngle: Double, linkLength: Double)
-
-    def createTrafo(link: Link): Frame =
+    def createTrafo(link: (Double, Double)): Frame =
       link match {
-        case Link(q, l) => Matrix(
-            (cos(q), -sin(q), cos(q) * l),
-            (sin(q), cos(q), sin(q) * l),
-            (0.0, 0.0, 1.0)
-          )
+        case (q, l) => Matrix(
+          (cos(q), -sin(q), cos(q) * l),
+          (sin(q), cos(q), sin(q) * l),
+          (0.0, 0.0, 1.0)
+        )
       }
 
-    def frameRec(links: List[Link], previous: Frame): List[Frame] =
+    def frameRec(links: List[(Double, Double)], previous: Frame): List[Frame] =
       links match {
         case Nil => previous :: Nil
         case link :: rest => previous :: frameRec(rest, previous * createTrafo(link))
       }
 
-    val links = (jointAngles zip linkLengths) map {
-      case (q, l) => Link(q, l)
-    }
-
-    frameRec(links, DenseMatrix.eye[Double](3))
+    frameRec(jointAngles zip linkLengths, DenseMatrix.eye[Double](3))
   }
 
-  def points(jointAngles: List[Double]): List[Vector[Double]] =
-    frames(jointAngles).map(frame => DenseVector(frame(0, 2), frame(1, 2)))
+  protected def extractPoints(frames: List[Frame]): List[Vector[Double]] =
+    frames map (frame => DenseVector(frame(0, 2), frame(1, 2)))
 
   def moveTo(jointAngles: List[Double]): RobotState =
     RobotState(linkLengths, jointAngles)
@@ -43,6 +39,6 @@ class Robot(linkLengths: List[Double]) {
 }
 
 case class RobotState(linkLengths: List[Double], jointAngles: List[Double]) extends Robot(linkLengths) {
-  lazy val frames: List[Frame] = super.frames(jointAngles)
-  lazy val points: List[Vector[Double]] = super.points(jointAngles)
+  lazy val frames: List[Frame] = frames(jointAngles)
+  lazy val points: List[Vector[Double]] = extractPoints(frames)
 }
